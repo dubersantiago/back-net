6603e753cecf0fde199005822c757e31b5b37eb4
dubersantiago <dubers1@gmail.com>
2026-07-13 22:00:46 -0500

    feat: add ApplicationDbContext and configure SQL Server connection

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var dbConectionString = builder.Configuration.GetConnectionString("ConexionSql");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(dbConectionString));
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
