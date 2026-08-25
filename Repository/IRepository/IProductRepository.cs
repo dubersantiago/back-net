a0e68644d45a50b2b193bdceb73b16c93b28a97f
dubersantiago <dubersi1@gmail.com>
2026-08-24 20:03:09 -0500

    feat: interfaz de repository de productos

---
+++ Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using back_net.Models;
+
+namespace back_net.Repository.IRepository;
+public interface IProductRepository
+{
+    ICollection<Product> GetProducts();
+    ICollection<Product> GetProductsForCategory(int categoryId);
+    ICollection<Product> SearchProduct(String nombre);
+    Product? GetProduct(int id);
+    Boolean BuyProduct(String nombre, int cantidad);
+    Boolean ProductExist(String nombre);
+    Boolean CreateProduct(Product product);
+    Boolean UpdateProduct(Product product);
+    Boolean DeleteProduct(Product product);
+    Boolean Save();
+}
