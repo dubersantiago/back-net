35445caa10db39fbd057f33c70fd33cd0e7ab4fd
dubersantiago <dubersi1@gmail.com>
2026-08-24 20:48:46 -0500

    feat: metodos iniciaels del controlador

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using back_net.Repository.IRepository;
 using AutoMapper;
+using back_net.Models.Dtos;
 
 namespace back_net.Controllers;
 
@@ -16,5 +17,30 @@ public class ProductController: ControllerBase
     {
         _productRepository=productRepository;
         _mapper=mapper;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetProducts()
+    {
+        var products = _productRepository.GetProducts();
+        var productDto = _mapper.Map<List<ProductDto>>(products);
+        
+        return Ok(productDto);
+    }
+
+    [HttpGet("{productId:int}",Name = "GetProduct")]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetProduct(int productId)
+    {
+        var product = _productRepository.GetProduct(productId);
+        if(product == null) return NotFound($"El producto con el id {productId} no existe");
+        var productDto = _mapper.Map<ProductDto>(product);
+        
+        return Ok(productDto);
     }
 }
